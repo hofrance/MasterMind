920e2804d7a19140c94014510f8f71e2f0e27581
hofrance <hofrancebankouezi@gmail.com>
2024-06-28 02:24:46 +0200

    correction de la fonction somme et ajout d\'un test

--- fonction3.py
+++ fonction3.py
@@ -1,3 +1,21 @@
 a=13
 b=2
 Somme=a+b
+
+#focntin qui fait la somme de deux nombres
+def somme-de-deux-nombres(premier_nombre,seconde_nombre):
+    """
+     cette foonciton prend deux paramettres en entré et renvoie leur sommes   
+    """
+    resulat=premier_nombre+seconde_nombre
+    return resulat
+
+
+
+#test
+a=4
+b=8
+
+result=somme-de-deux-nombres(a,b)
+print("le resultat est", resulat)
+#test fonction somme
